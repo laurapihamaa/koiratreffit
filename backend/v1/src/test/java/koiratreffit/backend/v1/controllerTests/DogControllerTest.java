0dfb216dd17889f9ac37edd50e7514baafd791f3
lmpihamaa <lpihamaa@volvocars.com>
2024-11-27 17:56:21 +0200

    add fixes to test for running pipeline

--- backend/v1/src/test/java/koiratreffit/backend/v1/controllerTests/DogControllerTest.java
+++ backend/v1/src/test/java/koiratreffit/backend/v1/controllerTests/DogControllerTest.java
@@ -7,9 +7,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -21,22 +25,17 @@ import koiratreffit.backend.v1.controllers.DogController;
 import koiratreffit.backend.v1.interfaces.DogServiceInterface;
 import koiratreffit.backend.v1.objects.Dog;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(DogController.class)
 public class DogControllerTest {
 
-    @InjectMocks
-    private DogController dogController;
-
-    @Mock
+    @MockBean
     private DogServiceInterface dogServiceInterface;
 
+    @Autowired
     private MockMvc mockMvc;
 
     @BeforeEach
     public void setUp() {
-        MockitoAnnotations.openMocks(this);
-        mockMvc = MockMvcBuilders.standaloneSetup(dogController).build();
     }
 
     @Test
@@ -44,6 +43,7 @@ public class DogControllerTest {
         Dog dog = new Dog();
         dog.setId(ObjectId.get()); 
         dog.setName("musti");
+        dog.setAge(5.5);
 
         when(dogServiceInterface.getDogDataById(dog.getId().toString())).thenReturn(dog);
 
